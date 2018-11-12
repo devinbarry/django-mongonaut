dea7e53ac73ac167ea5bde4df35fb51c5f282f2b
Devin Barry <devinbarry@gmail.com>
2018-11-12 22:36:24 +0000

    user.is_authenticated is not a function. Its a property.

--- mongonaut/mixins.py
+++ mongonaut/mixins.py
@@ -149,6 +149,7 @@ class MongonautViewMixin(object):
         context['has_edit_permission'] = self.mongoadmin.has_edit_permission(self.request)
         context['has_add_permission'] = self.mongoadmin.has_add_permission(self.request)
         context['has_delete_permission'] = self.mongoadmin.has_delete_permission(self.request)
+
         return context
 
 
